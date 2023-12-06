f955a6dbe663b8c3105c2a2cfb195a1a97603dea
Kara Woo <woo.kara@gmail.com>
2023-12-06 10:28:15 -0800

    Improve day 6

--- day06.R
+++ day06.R
@@ -6,25 +6,24 @@ dat <- read_lines("input06.txt") %>%
   lapply(as.numeric) %>%
   setNames(c("time", "duration"))
 
-race_result <- function(hold_time, duration) {
+race_result <- function(duration) {
+  hold_time <- 0:duration
   move_time <- duration - hold_time
   hold_time * move_time
-}
-
-get_possible_results <- function(duration) {
-  vapply(0:duration, race_result, numeric(1), duration = duration)
 }
 
 get_winning_times <- function(poss_results, record) {
   sum(poss_results > record)
 }
 
+get_answer <- function(dat) {
+  poss_results <- lapply(dat[["time"]], race_result)
+  prod(mapply(get_winning_times, poss_results, dat[["duration"]]))
+}
+
 ## Part 1
-poss_results <- lapply(dat[["time"]], get_possible_results)
-prod(mapply(get_winning_times, poss_results, dat[["duration"]]))
+get_answer(dat)
 
 ## Part 2
-dat2 <- lapply(dat, function(x) as.numeric(paste0(x, collapse = "")))
-
-poss_results2 <- get_possible_results(dat2[["time"]])
-prod(get_winning_times(poss_results2, dat2[["duration"]]))
+dat <- lapply(dat, function(x) as.numeric(paste0(x, collapse = "")))
+get_answer(dat)
